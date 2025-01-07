64e9ffa0e99d5621f7a5f099c518093b2858e233
Emma Mitchell <emmamitchell@Emmas-MacBook-Air-5.local>
2025-01-06 20:09:53 -0500

    added script for volume of the shark fins, using the equation for volume of a cone: V = (1.3)pi*r^2*h

---
+++ volumeEstimato.R
@@ -0,0 +1,23 @@
+# BIOL 432 
+#Assignment 1 - Version Control 
+
+#load packages
+library(dplyr)
+
+#read the measurement csv
+
+data<-read.csv("measurements.csv")
+
+#estimate volume - use cone volume formula V = (1/3)pi*r^2*h
+# r is the fin width/2 
+#h is the height (limb length)
+
+data <- data %>%
+  mutate(Volume = (1/3) * pi * (Fin_Width/2)^2 * Limb_Length)
+
+head(data)
+
+#csv
+write.csv(data, "measurements.csv", row.names = F)
+
+
